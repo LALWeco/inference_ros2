9f229472f7b67e601314c834ba299859139ec051
Naeem Iqbal <naeem.iqbal@dfki.de>
2025-04-04 18:37:36 +0200

    tmp commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-set -e
 
-# Source ROS 2 setup files
-source /opt/ros/$ROS_DISTRO/setup.bash
+# Source ROS setup files
+source /opt/ros/humble/setup.bash
 source /home/docker/ros2_ws/install/setup.bash
-# Execute the command passed to the docker run
-exec "$@"
+
+# Export the model path
+export MODEL_PATH=/home/docker/ros2_ws/src/inference_ros2/model
+
+# Check if a command is provided
+if [ "$#" -eq 0 ]; then
+    # No command provided, execute the default command
+    exec ros2 launch inference_ros2 tracking.launch.py params:=${CONFIG}
+else
+    # Command provided, execute it
+    exec "$@"
+fi
--- scripts/launch_docker.sh
+++ scripts/launch_docker.sh
@@ -5,8 +5,9 @@ docker run -it --runtime=nvidia \
   --ipc=host \
   -v /dev/shm:/dev/shm \
   -v /home/lero02/bags:/home/docker/bags:ro \
+  -v /home/niqbal/ros2_ws:/home/docker/ros_ws:ro \
   -e DISPLAY=$DISPLAY \
   --user $(id -u):$(id -g) \
   --name=crop_tracker_leroc2 \
   lalweco/crop_tracker:23.10-humble-py3 \
-  /bin/bash 
+  /bin/bash -c "source /opt/ros/humble/setup.bash && source /home/docker/ros_ws/install/setup.bash && export MODEL_PATH=/home/docker/ros_ws/src/inference_ros2/model && ros2 launch inference_ros2 tracking.launch.py params:=/home/docker/ros_ws/src/inference_ros2/inference_ros2/config/default_params.yaml"
