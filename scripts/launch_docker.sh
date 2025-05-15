f1f4c9a1c7af34cc7370655e1c5e227b3d02c191
Lero02 <lero@example.com>
2025-05-15 14:12:37 +0200

    Added Zenoh compatibility

--- scripts/launch_docker.sh
+++ scripts/launch_docker.sh
@@ -1,13 +1,12 @@
 docker run -it --runtime=nvidia \
-  --rm \
   --env TERM=xterm-256color \
   --network=host \
   --ipc=host \
   -v /dev/shm:/dev/shm \
   -v /home/lero02/bags:/home/docker/bags:ro \
-  -v /home/niqbal/ros2_ws:/home/docker/ros_ws:ro \
   -e DISPLAY=$DISPLAY \
+  -e RMW_IMPLEMENTATION=$RMW_IMPLEMENTATION \
+  -e ROS_DOMAIN_ID=$ROS_DOMAIN_ID \
   --user $(id -u):$(id -g) \
-  --name=crop_tracker_leroc2 \
-  lalweco/crop_tracker:23.10-humble-py3 \
-  /bin/bash -c "source /opt/ros/humble/setup.bash && source /home/docker/ros_ws/install/setup.bash && export MODEL_PATH=/home/docker/ros_ws/src/inference_ros2/model && ros2 launch inference_ros2 tracking.launch.py params:=/home/docker/ros_ws/src/inference_ros2/inference_ros2/config/default_params.yaml"
+  --name=lero_perception_lalweco \
+  lalweco/crop_tracker:23.10-humble-py3
